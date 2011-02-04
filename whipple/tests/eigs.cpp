fda1d346affad59569af40e25d7db39b09c39c7e
Dale Lukas Peterson <hazelnusse@gmail.com>
2011-02-03 22:52:23 -0800

    Fixed static library reference in Makefile, code cleanup.

--- whipple/tests/eigs.cpp
+++ whipple/tests/eigs.cpp
@@ -1,6 +1,5 @@
-/* eigs.cpp
- *
- * Copyright (C) 2010 Dale Lukas Peterson
+/*
+ * Copyright (C) 2011 Dale Lukas Peterson
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
